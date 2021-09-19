5488b3fe6cfdf2f1165e5188d070e763969093b5
Matteo Chiesa <83218727+Church-17@users.noreply.github.com>
2021-09-19 10:18:56 +0200

    Change beta logo

--- features/logo.js
+++ features/logo.js
@@ -10,10 +10,16 @@ chrome.storage.sync.get('logo', function(data) {
             background-image: url('/img/animated-doodle_600127bdb5f7627ede5cd4ef320f55b0.png');\
             background-position: 0;\
 			filter: contrast(0.6) saturate(1.8) brightness(0.9);\
+			width: 355px;\
+			height: 355px;\
+			background-size: 1600% auto;\
         }\
         @keyframes logo {\
             to {background-position-x: 100%}\
-        }"
+        }\
+		[data-icon='intro-md-beta-logo-dark'] > svg {\
+			display: none;\
+		}"
 	} else {
 		document.getElementById('changeLogo').innerHTML = ''
 	}
