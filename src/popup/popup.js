445b9e078bd94cbf5cd575857b5a4c53be9e1f4d
Matteo Chiesa <83218727+Church-17@users.noreply.github.com>
2022-02-26 17:52:48 +0100

    Add notify feature

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -3,13 +3,15 @@ var lineBack = document.getElementById('lineBack')
 var lineForth = document.getElementById('lineForth')
 var switchLogo = document.getElementById('switchLogo')
 var switchColor = document.getElementById('switchColor')
+var switchNotify = document.getElementById('switchNotify')
 
-chrome.storage.sync.get(['linePx','logo','color'], function(data) {
+chrome.storage.sync.get(['linePx','logo','color', 'notify'], function(data) {
 	lineNow.innerHTML = data.linePx / 20
 	lineBack.value = data.linePx
 	lineForth.value = data.linePx
 	switchLogo.checked = data.logo
 	switchColor.checked = data.color
+	switchNotify.checked = data.notify
 })
 
 lineBack.onclick = function() {
@@ -45,5 +47,12 @@ switchColor.onchange = function() {
     chrome.storage.sync.set({color: switchColor.checked})
     chrome.tabs.query({url: "*://web.whatsapp.com/"}, function(tabs) {
 		chrome.tabs.executeScript(tabs[0].id, {file: "feature/color.js"})
+    })
+}
+
+switchNotify.onchange = function() {
+    chrome.storage.sync.set({notify: switchNotify.checked})
+    chrome.tabs.query({url: "*://web.whatsapp.com/"}, function(tabs) {
+		chrome.tabs.executeScript(tabs[0].id, {file: "features/notify.js"})
     })
 }
