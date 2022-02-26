a7461f90a2e4fa203de4fe9c1fcf1ef34cc84a1c
Matteo Chiesa <83218727+Church-17@users.noreply.github.com>
2022-02-26 16:23:04 +0100

    Del teal

--- src/features/color.js
+++ src/features/color.js
@@ -32,7 +32,6 @@ chrome.storage.sync.get('color', function(data) {
 			--outgoing-background-deeper-rgb: 18,78,58;\
 			--progress-primary: var(---main);\
 			--round-icon-background: var(---main);\
-			--teal: var(---main);\
 			--typing: var(---lighter);\
 			--unread-marker-background: var(---light);\
 		}'
