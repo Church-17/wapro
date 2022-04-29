1a0feda04befd0af773b9a849c50808e6d162f6a
Matteo Chiesa <83218727+Church-17@users.noreply.github.com>
2022-04-29 18:17:15 +0200

    function executeNotify

--- src/features/notify.js
+++ src/features/notify.js
@@ -1,27 +1,25 @@
+executeNotify = function() {
+	chrome.storage.sync.get('notify', function(data) {
+		if (data.notify) {
+			document.getElementById('notify').style.height = 0
+		} else {
+			document.getElementById('notify').style = ''
+		}
+	})
+}
+
 if (document.getElementById('notify') == null) {
 	const targetNode = document.getElementById('app');
 	const callback = function(mutationsList, observer) {
 	    if (document.getElementById('side').children[1].tagName == 'SPAN') {
 			document.getElementById('side').children[1].id = 'notify';
-			chrome.storage.sync.get('notify', function(data) {
-				if (data.notify) {
-					document.getElementById('notify').style.height = 0;
-				} else {
-					document.getElementById('notify').style = '';
-				}
-			})
+			executeNotify()
 			observer.disconnect();
 		}
 	}
 	const observer = new MutationObserver(callback);
 	observer.observe(targetNode, {childList: true});
 } else {
-	chrome.storage.sync.get('notify', function(data) {
-		if (data.notify) {
-			document.getElementById('notify').style.height = 0;
-		} else {
-			document.getElementById('notify').style = '';
-		}
-	})
+	executeNotify()
 }
 	
