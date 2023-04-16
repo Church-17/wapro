312a54d5c6afed644fba16aba8b49041ece6a8b3
Matteo Chiesa <83218727+Church-17@users.noreply.github.com>
2023-04-16 12:13:00 +0200

    Add colors

--- src/modules/color.js
+++ src/modules/color.js
@@ -6,6 +6,8 @@ if (document.getElementById('id_color') == null) {
 chrome.storage.sync.get('color', function(data) {
 	if (data.color == true) {
 		document.getElementById('id_color').innerHTML = '.dark {\
+			---darker: #124e3a;\
+			---dark: #165a43;\
 			---main: #008c69;\
 			---light: #009c76;\
 			---lighter: #0fa587;\
@@ -19,16 +21,20 @@ chrome.storage.sync.get('color', function(data) {
 			--checkbox-background: var(---main);\
 			--highlight: var(---lighter);\
 			--icon-high-emphasis: var(---main);\
+			--icon-search-back: var(---main);\
 			--input-border-active: var(---main);\
-			--intro-border: #045f47;\
+			--intro-border: var(---dark);\
 			--menu-tabs-list-active: var(---main);\
-			--outgoing-background: #165a43;\
+			--outgoing-background: var(---dark);\
 			--outgoing-background-rgb: 22,90,67;\
-			--outgoing-background-deeper: #124e3a;\
+			--outgoing-background-deeper: var(---darker);\
 			--outgoing-background-deeper-rgb: 18,78,58;\
 			--poll-bar-fill-receiver: var(---light);\
 			--progress-primary: var(---main);\
+			--ptt-draft-button-send: var(---main);\
+			--ptt-draft-button-send-hover: var(---lighter);\
 			--round-icon-background: var(---main);\
+			--switch-button-checked-color: var(---main);\
 			--typing: var(---lighter);\
 			--unread-marker-background: var(---light);\
 		}'
